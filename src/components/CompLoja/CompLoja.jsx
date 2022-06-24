12be337e1913e450d1937bcbf80a50a266199304
guilherme de brida <guilhermedbdb@gmail.com>
2022-06-24 20:36:59 -0300

    loja

--- src/components/CompLoja/CompLoja.jsx
+++ src/components/CompLoja/CompLoja.jsx
@@ -1,46 +1,29 @@
 import { Card, Col, Row } from 'antd';
+import Item from 'antd/lib/list/Item';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { CompFunkoCard } from '../CompFunkoCard/CompFunkoCard';
 
-export const CompLoja = () => (
+export const CompLoja = () => {
+     const [funko, setFunko] = useState(null)
+     useEffect(()=>{
+          const pegarFunkos = async()=>{
+               const {data}=await axios.get("http://localhost:5000/api/funko/") 
+               setFunko(data);
+          }
+          pegarFunkos();
+     }, [])
+
+     return (
+<>
+  
+
   <div className="site-card-wrapper">
-    <Row gutter={16}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      <Col span={8}>
-      <Col span={8}>
- <CompFunkoCard/>
-      </Col>
-      </Col>
-    </Row>
+    { funko.map((item) => {
+          <CompFunkoCard url={item.url} descricao={item.descricao} valor={item.valor}/>
+     })}
   </div>
-);
+  </>
+)
+};
