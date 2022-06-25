5314aba57996495d3e8413787ddc989a23e5be7a
ViniciusFCamilo <fcvini@gmail.com>
2022-06-25 15:50:33 -0300

    GetMeuUsuario

--- src/components/CompMeuUsuario/CompMeuUsuario.jsx
+++ src/components/CompMeuUsuario/CompMeuUsuario.jsx
@@ -1,11 +1,24 @@
 import { Col, Radio, Row, Tabs } from 'antd';
 import Column from 'antd/lib/table/Column';
+import axios from 'axios';
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { CompCardMeuUsuario } from '../CompCardMeuUsuario/CompCardMeuUsuario';
+import { CompFormFunko } from '../CompFormFunko/CompFormFunko';
+import { CompFunkoCard } from '../CompFunkoCard/CompFunkoCard';
 
 const { TabPane } = Tabs;
 
 export const CompMeuUsuario = () => {
+  const [usuario, setUsuario] = useState(null)
+     useEffect(() => {
+          const pegarUsuario = async () => {
+            const { data } = await axios.get("http://localhost:5000/api/usuario/62b3a8b87991ed0682350e7d")
+            setUsuario(data);
+          }
+          pegarUsuario();
+     }, [])
+     console.log(usuario)
   const [mode, setMode] = useState('left');
 
   const handleModeChange = (e) => {
@@ -13,6 +26,7 @@ export const CompMeuUsuario = () => {
   };
 
   return (
+    <>
     <div>
       <Tabs
         defaultActiveKey="1"
@@ -23,16 +37,17 @@ export const CompMeuUsuario = () => {
         }}
       >
         {<TabPane tab={'Perfil'} key={1}>
-            <Row>
-                {<Col span={24}>
-                    <CompCardMeuUsuario/>
-                </Col>} 
-            </Row>
+            <CompCardMeuUsuario nome={usuario?.nome} user={usuario?.user} senha={usuario?.senha}/>
         </TabPane>}
         {<TabPane tab={'Lista de Funkos'} key={2}>
-            Salve
+          <Row>
+            {usuario?.funkos?.map((item) => (
+              <CompFunkoCard url={item.url} descricao={item.descricao} valor={item.valor} />
+            ))}
+          </Row>  
         </TabPane>}
       </Tabs>
     </div>
+    </>
   );
 };
