426613a02d1ad4722213528819b76e20c969c9e2
guilherme de brida <guilhermedbdb@gmail.com>
2022-06-26 20:39:12 -0300

    deleteFunko

--- src/components/CompExcluirUsuario/CompExcluirUsuario.jsx
+++ src/components/CompExcluirUsuario/CompExcluirUsuario.jsx
@@ -2,7 +2,6 @@ import { Button } from 'antd';
 import axios from 'axios';
 
 export const CompExcluirUsuario = () => {
-
     return(
         <>
          <Button type="primary">Excluir Usuário</Button>
