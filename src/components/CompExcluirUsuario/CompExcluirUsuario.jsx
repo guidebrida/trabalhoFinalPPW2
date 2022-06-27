1a9b10dde99e0c28f68c6d73b98cb0ba4515afea
Misael Cordova <misael.cordova.f@gmail.com>
2022-06-26 23:05:58 -0300

    delete usuario

--- src/components/CompExcluirUsuario/CompExcluirUsuario.jsx
+++ src/components/CompExcluirUsuario/CompExcluirUsuario.jsx
@@ -1,10 +1,28 @@
 import { Button } from 'antd';
 import axios from 'axios';
+import { useContext,useState} from 'react';
+import LoginContext from '../../context/ContextLogin';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 
 export const CompExcluirUsuario = () => {
+    const {login} = useContext(LoginContext)
+    const [status, setStatus] = useState()
+    const navegacao = useNavigate()
+    const deletar = async (values) => {  
+        console.log(login.id)
+        try{
+          const Result = await axios.delete(`http://localhost:5000/api/usuario/${login.id}`)
+          setStatus(Result.status);
+          navegacao('/Login')
+        }catch(error){
+          setStatus(error.request.status)
+        }
+    
+        };
     return(
         <>
-         <Button type="primary">Excluir Usuário</Button>
+         <Button onClick={deletar} type="primary">Excluir Usuário</Button>
         </>
     )
 };
